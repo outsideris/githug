6e31388eb612c3bef678921f2451be546220100c
Outsider <outsideris@gmail.com>
2013-07-20 21:06:34 +0900

    added install page on non-standalone mode
    * added zepto.js
    * added pure.css
    * added font-awesome
    * google web font

---
+++ js/mobile.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2013 JeongHoon Byun aka "Outsider", <http://blog.outsider.ne.kr/>
+ * Licensed under the MIT license.
+ * <http://outsider.mit-license.org/>
+ */
+$(document).ready(function() {
+  if (!window.navigator.standalone) {
+    $('#install').show();
+    $('#loading').hide();
+  }
+});
