4ffe01f42bb12b6f65cf767eb42c7601477baa13
Outsider <outsideris@gmail.com>
2013-08-29 22:55:32 +0900

    removed the install page
    * installing page as app is supported only iOS

--- app/js/app.js
+++ app/js/app.js
@@ -9,39 +9,34 @@
   angular.module('githug', ['ngResource'])
     .config(['$routeProvider', function($routeProvider) {
       $routeProvider.
-        when('/install', {templateUrl: 'partials/install.html'}).
         when('/signin', {templateUrl: 'partials/signin.html', controller: SignInCtrl}).
         when('/timeline', {templateUrl: 'partials/timeline.html', controller: TimelineCtrl}).
         otherwise({redirectTo: '/'});
     }])
     .run(function($location, env, githubService) {
-      if (!window.navigator.standalone) {
-        $location.path('/install');
+      var isSignIn = localStorage.getItem('token');
+      if (isSignIn) {
+        $location.path('/timeline');
       } else {
-        var isSignIn = localStorage.getItem('token');
-        if (isSignIn) {
-          $location.path('/timeline');
-        } else {
-          $location.path('/signin');
-        }
-
-        // TODO: signin page show up when url redirected from oauth.io
-        OAuth.callback('github', function(err, result) {
-          if (err) { throw new Error(err); }
-
-          env.user('token', result.access_token);
-          env.user('tokenType', result.token_type);
-
-          githubService.MyUserInfo()
-            .get(function(data) {
-              env.user('name', data.name);
-              env.user('userid', data.login);
-              env.user('avatar', data.avatar_url);
-              env.user('apiurl', data.url);
-
-              $location.path('/timeline');
-            });
-        });
+        $location.path('/signin');
       }
+
+      // TODO: signin page show up when url redirected from oauth.io
+      OAuth.callback('github', function(err, result) {
+        if (err) { throw new Error(err); }
+
+        env.user('token', result.access_token);
+        env.user('tokenType', result.token_type);
+
+        githubService.MyUserInfo()
+          .get(function(data) {
+            env.user('name', data.name);
+            env.user('userid', data.login);
+            env.user('avatar', data.avatar_url);
+            env.user('apiurl', data.url);
+
+            $location.path('/timeline');
+          });
+      });
     });
 })();
