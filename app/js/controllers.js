bcfed35977cb5948994c3ae0654fc0cfc8233611
Outsider <outsideris@gmail.com>
2013-09-10 22:15:26 +0900

    fixed parse error when link header is absent
    * in case of subscribers(watch) are inner one page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -126,7 +126,8 @@ function RepositoryCtrl($scope, $routeParams, githubService, commonService) {
 
   // watch count
   RepoWatchers.query(function(data, getResponseHeaders) {
-    var lastSubscribersUrl = commonService.parseLinkHeader(getResponseHeaders('link')).last,
+    var lastSubscribersUrl = getResponseHeaders('link') ?
+                             commonService.parseLinkHeader(getResponseHeaders('link')).last : '?page=0',
         paramOfLastSubscribersUrl = lastSubscribersUrl.substr(lastSubscribersUrl.indexOf('?')),
         lastPage = commonService.getParameterByName(paramOfLastSubscribersUrl, 'page');
 
